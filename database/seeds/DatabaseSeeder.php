5757413ccef92078bcc8c240c523897d9445168d
dmbartles <dmbartles@yahoo.com>
2017-12-12 21:39:45 -0500

    final commit2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,8 +7,8 @@ class DatabaseSeeder extends Seeder
   {
 
     $this->call(PostsTableSeeder::class);
-    $this->call(CommentsTableSeeder::class);
     $this->call(TagsTableSeeder::class);
+    $this->call(CommentsTableSeeder::class);
     $this->call(PostTagTableSeeder::class);
 
   }
